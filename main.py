132ffd4b8a44e2820122f0cd9950bed5fabde29a
inst <safonovartem829@gmail.com>
2023-04-05 12:29:07 +0300

    Create new project

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ my_file = open('CheckList.txt', "r")
 if not os.path.isdir("List of characters"):#Проверка наличия каталога
     os.mkdir("List of characters")#Создание каталога
 
-os.chdir("List of characters") #Смена директории
+os.chdir("List of characters") #Смена директория
 
 for i in range(0,43):
     for line in my_file:
