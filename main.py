5dc5df266f9531b82b2c7747e0c423ec4451594e
inst <safonovartem829@gmail.com>
2023-04-05 12:27:12 +0300

    Create new project

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+my_file = open('CheckList.txt', "r")
+
+
+if not os.path.isdir("List of characters"):#Проверка наличия каталога
+    os.mkdir("List of characters")#Создание каталога
+
+os.chdir("List of characters") #Смена директории
+
+for i in range(0,43):
+    for line in my_file:
+        if not os.path.isdir('{}'.format(str(line.strip()))):
+             os.mkdir('{}'.format(str(line.strip())))
+        break
